0b2f51bedb320d71a659a2755c58df5d58297438
kf-cuanschutz <127144640+kf-cuanschutz@users.noreply.github.com>
2024-07-22 22:46:53 -0600

    Create exp_hplx_gpu.sh
    All the matrix sizes in 1

---
+++ Experiments/HPL_scripts/exp_hplx_gpu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mpirun -n 1 /workspace/hpl-mxp.sh --n 10000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_10000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 20000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_20000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 30000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_30000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 40000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_40000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 50000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_50000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 60000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_60000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 70000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_70000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 80000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_80000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 90000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_90000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 100000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_100000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 110000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_110000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 120000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_120000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 13000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_130000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 140000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_140000_.log
+mpirun -n 1 /workspace/hpl-mxp.sh --n 150000 --nb 2048 --nprow 1 --npcol 1 --nporder row --gpu-affinity 0:1:2:3:4:5:6:7 > hpl_mxp_mat_150000_.log
